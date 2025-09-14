ccfda60b24e3993920e9b7a28d08e9892c4698e1
Nayan <nayanunni95@gmail.com>
2025-09-14 22:43:27 +0530

    Initial Commit 🐛

---
+++ src/components/Map.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Map() {
+  return (
+    <div className="m-4 md:m-8 xl:mx-20">
+      <iframe
+        src=""
+        width="600"
+        height="450"
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        className="w-full h-[30rem] rounded-md map"
+      ></iframe>
+    </div>
+  )
+}
+
+export default Map
