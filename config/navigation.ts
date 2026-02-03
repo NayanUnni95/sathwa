acb2adb588ef5616ee520da558e65bb4e71e3265
Nayan <nayanunni95@gmail.com>
2026-02-03 13:25:08 +0530

    chore: update socialitems

--- config/navigation.ts
+++ config/navigation.ts
@@ -3,8 +3,10 @@ export const menuConfig = {
     { label: "Home", ariaLabel: "Home", link: "/" },
     { label: "Events", ariaLabel: "Events", link: "/events" },
     { label: "Contact", ariaLabel: "Contact", link: "/contact" },
+    { label: "Competitions", ariaLabel: "Competitions", link: "/competitions" },
   ],
   socialItems: [
     { label: "Instagram", link: "https://instagram.com/sathwa_cem" },
+    // { label: "Email", link: "mailto:sathwa26@gmail.com" },
   ],
 };
